640ad21ec0853155f29278f55318566c146b28db
coffeecup827 <coffeecup827@gmail.com>
2018-03-18 20:16:39 +0530

    Simple Layout Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  componentWillMount(){
+    this.setState({
+      Sentence : "",
+      JumbledWords : [],
+      enableUpdateButton : false,
+      showJumbledWords : false,
+      sentenceLength : 0,
+      wordsRemaining : 0
+    });
+  }
+
+  updateSentence = (e) => {
+    this.setState({
+      Sentence : e.target.value,
+      enableUpdateButton : e.target.value.split(" ").length > 1,
+      showJumbledWords : false
+    });
+  }
+
+  jumbleWords = () => {
+
+    var wordArray = this.state.Sentence.split(" ")
+
+    this.setState({
+      showJumbledWords : true,
+      JumbledWords : wordArray,
+      sentenceLength : wordArray.length,
+      wordsRemaining : wordArray.length
+    });
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col s12 m12">
 
-          <div className="card  light-blue darken-2">
-            <div className="card-content white-text">
+          <div className="card  grey lighten-5 z-depth-3">
+            <div className="card-content">
 
               <h3 className="center">Pick The Words In Order</h3>
 
-              
+              <div className="card-content">
+
+                <div id="inputBlock" className="row">
+
+                  <div className="input-field col s12 m6">
+                    <input
+                      placeholder="Your Sentence Here"
+                      id="sentence"
+                      type="text"
+                      onChange={(event) => {this.updateSentence(event)}}
+                     />
+                    <label htmlFor="sentence">Sentence</label>
+                  </div>
+
+                  <div
+                    id="updateButton"
+                    disabled={!this.state.enableUpdateButton}
+                    className="waves-effect waves-light btn" onClick={() => {this.jumbleWords()}}
+                  >
+                    UPDATE
+                  </div>
+
+                </div>
+
+                {
+                  this.state.showJumbledWords &&
+
+                    <div>
+                      <p>{this.state.Sentence + " " + this.state.wordsRemaining}</p>
+                    </div>
+
+                }
+
+              </div>
 
             </div>
           </div>
